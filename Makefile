598037bb558d0a6d705c09018597a8513e6faede
Pavel Ershov <devnulpavel@gmail.com>
2021-04-15 14:31:49 +0300

    Credentials refactoring

--- Makefile
+++ Makefile
@@ -8,31 +8,28 @@ BUILD_RELEASE:
 	cargo build --release
 
 ENCRYPT_TEST_ENV:
-	# -a: ASCII
-	# -r: Key fingerpring
-	# -e: Encrypt file
-	# -o: Output file
 	gpg -a -r 0x0BD10E4E6E578FB6 -o test_environment.env.asc -e test_environment.env
 
 DECRYPT_TEST_ENV:
-	# -a: ASCII
-	# -r: Key fingerpring
-	# -d: Encrypt file
-	# -o: Output file
 	gpg -a -r 0x0BD10E4E6E578FB6 -o test_environment.env -d test_environment.env.asc
 
 ENCRYPT_AUTH:
 	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_drive_my_auth.json.asc -e test_google_drive_my_auth.json
+	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_play_classic_auth.json.asc -e test_google_play_classic_auth.json
+	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_play_auth.json.asc -e test_google_play_auth.json
+
 
 DECRYPT_AUTH:
 	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_drive_my_auth.json -d test_google_drive_my_auth.json.asc
+	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_play_classic_auth.json -d test_google_play_classic_auth.json.asc
+	gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_play_auth.json -d test_google_play_auth.json.asc
 
 BENCH:
-	$(shell gpg -a -r 0x0BD10E4E6E578FB6 -d test_environment.env.asc) && \
+	source test_environment.env && \
 	cargo run --release --features=flame_it
 
 TEST_ENV_PARAMETERS:
-	$(shell gpg -a -r 0x0BD10E4E6E578FB6 -d test_environment.env.asc) && \
+	source test_environment.env && \
 	cargo test -- env_parameters::tests
 
 EXAMPLE_APP_PARAMETERS:
--- test_uploading.sh
+++ test_uploading.sh
@@ -1,20 +1,16 @@
 #! /usr/bin/env bash
 
-# Json auth
-rm -rf test_google_drive_my_auth.json
-gpg -a -r 0x0BD10E4E6E578FB6 -o test_google_drive_my_auth.json -d test_google_drive_my_auth.json.asc
-
 # Import test environment
-eval "$(gpg -a -r 0x0BD10E4E6E578FB6 -d test_environment.env.asc)"
+# eval "$(gpg -a -r 0x0BD10E4E6E578FB6 -d test_environment.env.asc)"
 # gpg -a -r 0x0BD10E4E6E578FB6 -d test_environment.env.asc -o test_environment.env
-# source test_environment.env
+source test_environment.env
 
 # Print environment
 # env | grep -i "GOOGLE_DRIVE"
 
 # Rust environment setup
-export RUST_BACKTRACE=1
 # export RUST_LOG=uploader_app=trace
+export RUST_BACKTRACE=1
 export RUST_LOG=uploader_app=trace,app_center_client=trace,google_drive_client=trace,reqwest=trace
 
 # App center
@@ -28,8 +24,15 @@ export RUST_LOG=uploader_app=trace,app_center_client=trace,google_drive_client=t
 
 # Google drive
 # --google_drive_target_domain ""
-target/debug/uploader_app \
-    --google_drive_files "/Users/devnul/Downloads/Mimestream_0.9.2.dmg" \
-    --google_drive_target_folder_id "1YtSfyiMp-MxF5AVWq_VnJxGtAwiMghBF" \
-    --google_drive_target_subfolder_name "NewFolderTest" \
-    --google_drive_target_owner_email "devnulpavel@gmail.com"
+# target/debug/uploader_app \
+#     --google_drive_files "/Users/devnul/Downloads/Mimestream_0.9.2.dmg" \
+#     --google_drive_target_folder_id "1YtSfyiMp-MxF5AVWq_VnJxGtAwiMghBF" \
+#     --google_drive_target_subfolder_name "NewFolderTest" \
+#     --google_drive_target_owner_email "devnulpavel@gmail.com"
+
+# Google play
+UPLOAD_PARAMS=()
+UPLOAD_PARAMS+=(--google_play_upload_file '/Users/devnul/Downloads/IslandTales-universal-gplay-12.3.0-42-14042021_1827-10c4f698/IslandTales-universal-gplay-12.3.0-42-14042021_1827-10c4f698.aab')
+UPLOAD_PARAMS+=(--google_play_target_track 'internal')
+UPLOAD_PARAMS+=(--google_play_package_name 'com.gameinsight.gplay.islandtales')
+target/debug/uploader_app "${UPLOAD_PARAMS[@]}"
